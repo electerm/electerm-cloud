6641a900d7f567b95258b4ab68d052e99667380f
ZHAO Xudong <zxdong@gmail.com>
2024-09-05 14:03:35 +0800

    Update language select UI

--- src/client/locales/lang-select.tsx
+++ src/client/locales/lang-select.tsx
@@ -1,29 +1,31 @@
 // lang-select.ts
 import React, { useState } from 'react'
 import { getLang } from './lang'
-import { Select } from 'antd'
+import { Radio } from 'antd'
+import type { RadioChangeEvent } from 'antd'
 
-const { Option } = Select
+const languages = [
+  { value: 'en', label: 'English' },
+  { value: 'zh', label: '中文' }
+]
 
 export default function LangSelect (): JSX.Element {
   const [lang, setLang] = useState(getLang())
-  function handleChange (value: string): void {
+  function handleChange (e: RadioChangeEvent): void {
+    const { value } = e.target
     ;(window as any).lang = value
     window.localStorage.setItem('lang', value)
     setLang(value)
     window.location.reload()
   }
-  const sleProps = {
-    value: lang,
-    onChange: handleChange
-  }
   return (
-    <Select
-      {...sleProps}
+    <Radio.Group
+      value={lang}
+      onChange={handleChange}
+      optionType='button'
+      buttonStyle='solid'
       size='small'
-    >
-      <Option value='en'>English</Option>
-      <Option value='cn'>中文</Option>
-    </Select>
+      options={languages}
+    />
   )
 }
